54b1c89c8c0750ea93e3fe8f9050268812eef031
Lolotron81 <a_manuel.leal.medina@iespablopicasso.es>
2023-11-27 19:59:51 +0100

    Hemos creado una nueva rama y modificado el programa de rama1

--- src/eedd/tarea3git/IntervaloIncremento7.java
+++ src/eedd/tarea3git/IntervaloIncremento7.java
@@ -8,19 +8,29 @@ public class IntervaloIncremento7 {
     
     Scanner sc = new Scanner(System.in);
     
-    // Solicitar y validar el segundo número (debe ser distinto al primero)
     int numero1;
     int numero2;
+ 
     do {
-      // Solicitar y validar el primer número
       System.out.print("Introduce el primer número: ");
       numero1 = Integer.parseInt(sc.nextLine());
       System.out.print("Introduce el segundo número (distinto al primero): ");
       numero2 = Integer.parseInt(sc.nextLine());
     } while (numero1 == numero2 || numero2 < numero1);
     
+    System.out.print("Introduce el tercer número: ");
+    int numero3 = Integer.parseInt(sc.nextLine());
+    
     for (int i = numero1; i <= numero2; i += 7) {
-      System.out.println(i);
+      if (i != numero3) {
+        System.out.println(i);
+      } else {
+        System.out.println(numero3);
+      }
+    }
+    
+    if (numero3 >= numero1 && numero3 <= numero2) {
+      System.out.println(numero3);
     }
 
     sc.close();
